6efe2a079d8c25d795aaae368a8d742b4de6d6d6
veerprabakar <prabakar.veer@gmail.com>
2017-09-16 20:26:30 +0800

    dirt kendo

---
+++ bulkupload/TelerikMvcApp1/TelerikMvcApp1/Controllers/FormsJsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelerikMvcApp1.Models;
+
+namespace TelerikMvcApp1.Controllers
+{
+    public class FormsJsController : Controller
+    {
+        // GET: FormsJs
+        public ActionResult Index()
+        {
+            return View(GetAll());
+        }
+
+        public IList<Form3A> GetAll()
+        {
+            var result = new List<Form3A>();
+            return result;
+        }
+    }
+}
---
+++ bulkupload/TelerikMvcApp1/TelerikMvcApp1/Models/Form3A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvcApp1.Models
+{
+    public class Form3A
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string RepNumber { get; set; }
+
+        public string RepName { get; set; }
+
+        public string NRIC { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Passport { get; set; }
+
+        public ICollection<Activity> Activities { get; set; }
+
+        public Form3A()
+        {
+            Activities = new List<Activity>();
+        }
+    }
+}
